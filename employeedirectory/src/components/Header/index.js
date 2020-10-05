0dbc1d9aaac4d220a28f5a736fed593d289c09db
Corvus <wyatta.hancock@gmail.com>
2020-10-05 11:20:18 -0600

    built header for the website

---
+++ employeedirectory/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+//This will build the Header for the page 
+function Header(){
+  return (
+    <div className="header">
+    <h1>Employee Directory</h1>
+    <p>Use the search box to narrow your results.</p>
+  </div>
+  )
+}
+
+export default Header
