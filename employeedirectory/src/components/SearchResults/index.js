b5250a1715c4c7ea7ca8c585d35e25ec6bc18812
Corvus <wyatta.hancock@gmail.com>
2020-10-07 10:20:41 -0600

    weird issue with search

--- employeedirectory/src/components/SearchResults/index.js
+++ employeedirectory/src/components/SearchResults/index.js
@@ -10,15 +10,23 @@ function SearchResults(props) {
                 <th scope="col">Image</th>
                 <th scope="col">First Name</th>
                 <th scope="col">Last Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Age</th>
+                <th scope="col">Cell</th>
+                <th scope="col">Email</th>
             </tr>
         </thead>
         <tbody>
             {props.results.map( (employee, index) => (
-                <tr>
-                    <th scope="row">{index+1}</th>
-                    <td><img src={employee.picture.thumbnail} alt="img of chosen employee"></img></td>
-                    <td>{employee.name.first}</td>
-                    <td>{employee.name.last}</td>
+                <tr key={employee.email}>
+                  <th scope="row">{index+1}</th>
+                  <td><img src={employee.picture.thumbnail} alt="img of employee"></img></td>
+                  <td>{employee.name.first}</td>
+                  <td>{employee.name.last}</td>
+                  <td>{employee.gender}</td>
+                  <td>{employee.dob.age}</td>
+                  <td>{employee.cell} </td>
+                  <td>{employee.email}</td>
                 </tr>
                 ))} 
         </tbody>
